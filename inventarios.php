00cf05c6821f96221d7320ee0e8b2ef1a3ae392d
sarahSBSG <ays11112022@gmail.com>
2024-11-20 15:46:54 -0600

    sigo en diseño de inventarios registro de productos

--- inventarios.php
+++ inventarios.php
@@ -12,47 +12,78 @@
         die();
     }
 ?>
-<!--INICIO DE PAGINA INVENTARIOS CON COMANDOS BASICOS-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INVENTARIOS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="formats/css/estilos_inv.css">
 </head>
-<body>   
+<body>
 
-<!--HEADER PARA DEFINIR LA CABECERA DE UNA PAGINA-->
-<header>
-<!--DIV CON UNA CLASE LLAMADA BACK PARA COLOR DE FONDO DE LA BARRA-->
-    <div class="back">
-<!--DIV CON UNA CLASE LLAMADA MENU_CONTAINER PARA LA BARRA-->
-        <div class="menu container">
-<!--ENLACE SIGNO NUMERAL PARA SIMUILAR UN ENLACE CON UNA CLASE LLAMADA LOGO--> 
-        <a href="#" class="logo"> Logo</a>
-<!--INPUT PARA LA PARTE RESPONSIVA-->
-        <input type="checkbox" id="menu" />
-<!--LABEL QUE HACE REFERENCIA AL ID MENU, AGREGANDO UNA IMAGEN CON SU CLASE-->
-        <label for="menu">
-            <img src="formats/images/menu.png" class="menu-icono" alt="">
-        </label>
+<div class="container-fluid row">
+<form class="col-4">
+  <fieldset disabled>
+    <legend class="text-center p-3">PRODUCTOS</legend>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Id Producto</label>
+      <input type="text" class="form-control" placeholder="Id" name="Id">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Nombre del Producto</label>
+      <input type="text" class="form-control" placeholder="Nombre" name="nom_producto">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Peso</label>
+      <input type="text" class="form-control" placeholder="Peso" name="peso">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Color</label>
+      <input type="text" class="form-control" placeholder="Color" name="color">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledSelect" class="form-label">Tamaño</label>
+      <select id="disabledSelect" class="form-select">
+        <option>Tamaño</option>
+      </select>
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Codigo de Barras</label>
+      <input type="text" class="form-control" placeholder="Codigo" name="codigo_barras">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Vendedor</label>
+      <input type="text" class="form-control" placeholder="Vendedor" name="vendedor">
+    </div>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Comprador</label>
+      <input type="text" class="form-control" placeholder="Comprador" name="comprador">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Subir</button>
+
 <!--NAVEGACION CON UNA CLASE DE NOMBRE NAVBAR Y SE CREA UNA LISTA DESORDENADA-->
-            <nav class="navbar">
-                <ul>   
-                    <li><a href="php/inicio.php">Inicio</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Contacto</a></li>
-                    <!--BOTON PARA CERRAR SESSION--> 
-                    <li><a href="php/cerrar_sesion.php">Cerrar Sesion</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>    
-</header>
 
+        <ul>
+            <!-- BOTÓN PARA CERRAR SESIÓN -->
+            <li>
+                <a href="php/cerrar_sesion.php" class="boton">Cerrar Sesión</a>
+            </li>
+        </ul>
+  </fieldset>
+  
+</form>
+</div>
 </body>
 </html>
-
-
